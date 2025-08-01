781f19faed69f0cb0b036f90f452739516676d11
Joel Thaduri <108522134+Jeoml@users.noreply.github.com>
2025-08-01 17:38:57 +0530

    Adding order number constraints

--- app/api/agent.tsx
+++ app/api/agent.tsx
@@ -133,8 +133,8 @@ const ProjectOverview = ({
             <span className="text-blue-600 dark:text-blue-400">{description}</span>
             ) : (
             <span>
-              <span className="text-red-500 font-semibold">If you get a 500 Server Error</span>
-                <span className="text-neutral-700 dark:text-neutral-300">, don't worry! My <u>Demo Server</u> sleeps after inactivity, so it takes a moment to wake up.</span>
+              <span className="text-red-500 font-semibold">Only orders 1 to 50 exist in demo database</span>
+                <span className="text-neutral-700 dark:text-neutral-300">If you get an error, don't worry! My <u>Demo Server</u> sleeps after inactivity, so it takes a moment to wake up.</span>
               <br />
               <span className="text-neutral-700 dark:text-neutral-300">Refresh ⏭️ Say Hi to wake it up ⏭️ Ask Anything.</span>
               <span className="text-yellow-500">⏳</span>
