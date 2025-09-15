1f973c2f406d418a190654dc309add21f449acb2
Joel Thaduri <108522134+Jeoml@users.noreply.github.com>
2025-09-16 00:25:44 +0530

    pls

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
-import { getUser, getTeamForUser } from '@/lib/db/queries';
-import { SWRConfig } from 'swr';
 import { NextAuthProvider } from './providers';
-import { GithubSignInButton, GoogleSignInButton } from '@/components/ui/authButtons';
 import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
@@ -29,20 +26,6 @@ export default function RootLayout({
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
       <body className="min-h-[100dvh] bg-gray-50">
-        {/* <SWRConfig
-          value={{
-            fallback: {
-              // We do NOT await here
-              // Only components that read this data will suspend
-              '/api/user': getUser(),
-              '/api/team': getTeamForUser()
-            }
-          }}
-        >
-          {children}
-        </SWRConfig> */}
-        {/* <GoogleSignInButton />
-        <GithubSignInButton /> */}
         <NextAuthProvider>
           {/* <TwitterNavbar /> */}
           {children}
